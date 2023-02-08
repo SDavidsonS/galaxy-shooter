fd09a601be0dbf45b4aef3635bf498b9d3ec6311
DESKTOP-RKKAM5F\Happy <wallaced2009@hotmail.com>
2023-02-08 14:51:37 -0300

    Movimento do player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveY= Input.GetAxis("Horizontal");
+        var moveX= Input.GetAxis("Vertical");
+        print(moveX);
+        transform.Translate(Vector3.up * Speed * Time.deltaTime * moveX);
+        transform.Translate(Vector3.right * Speed * Time.deltaTime * moveY );
+    }
+}
