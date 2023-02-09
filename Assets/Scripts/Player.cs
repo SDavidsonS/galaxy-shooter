fefb3caf245365674d2880e47a00e41698063feb
DESKTOP-RKKAM5F\Happy <wallaced2009@hotmail.com>
2023-02-09 14:28:07 -0300

    Limita as bordas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,10 +15,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var moveY= Input.GetAxis("Horizontal");
-        var moveX= Input.GetAxis("Vertical");
-        print(moveX);
+        var moveY = Input.GetAxis("Horizontal");
+        var moveX = Input.GetAxis("Vertical");
         transform.Translate(Vector3.up * Speed * Time.deltaTime * moveX);
-        transform.Translate(Vector3.right * Speed * Time.deltaTime * moveY );
+        transform.Translate(Vector3.right * Speed * Time.deltaTime * moveY);
+
+        if (transform.position.x > 8.858218f)
+
+        {
+            var newposition = transform.position;
+            newposition.x = 8.858218f;
+            transform.position = newposition;
+        }
+
+        if (transform.position.y > 5.724001f)
+        {
+            var newposition = transform.position;
+            newposition.y = 5.724001f;
+            transform.position = newposition;
+        }
+
+        if (transform.position.x < -8.773135f)
+        {
+            var newpositon = transform.position;
+            newpositon.x = -8.773135f;
+            transform.position = newpositon;
+        }
+
+        if (transform.position.y < -3.715358f)
+        {
+            var newposition = transform.position;
+            newposition.y = -3.715358f;
+            transform.position = newposition;
+        }
+        
     }
 }
