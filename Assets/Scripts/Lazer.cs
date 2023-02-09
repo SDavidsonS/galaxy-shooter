5130e8bf0a0e8a500254a4a485cb8f6625b390b3
DESKTOP-RKKAM5F\Happy <wallaced2009@hotmail.com>
2023-02-09 14:50:53 -0300

    Prefab do lazer

---
+++ Assets/Scripts/Lazer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Lazer : MonoBehaviour
+{
+    public float Speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Speed * Time.deltaTime);
+    }
+}
